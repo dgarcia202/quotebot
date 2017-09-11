1d5a5dea8659aa507f591dd60f1066f38d22b806
Diego García <wft2000@gmail.com>
2017-09-11 12:33:00 +0200

    handly func for interval times

--- src/config.js
+++ src/config.js
@@ -1,10 +1,19 @@
 require('dotenv').config();
 
+function hours(num) {
+  return 3600000 * num;
+}
+
+function minutes(num) {
+  return 60000 * num;
+}
+
 module.exports = {
   consumer_key: process.env.CONSUMER_KEY,
   consumer_secret: process.env.CONSUMER_SECRET,
   access_token: process.env.ACCESS_TOKEN,
   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
   start_http: false,
-  quote_interval: 3600000 /* That's one hour */
+  quote_interval: hours(10),
+  followback_interval: minutes(20)
 }
